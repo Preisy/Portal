c90220c03aa03081ef9a3e218f873e39a371fd60
max2556 <46759632+max2556@users.noreply.github.com>
2023-07-20 14:55:47 +0300

    Fix: removed cringe review

--- src/components/reviews/ReviewCardsData.ts
+++ src/components/reviews/ReviewCardsData.ts
@@ -2,7 +2,6 @@ import type {ReviewCard} from "@/components/reviews/ReviewCard";
 import review_1 from "@/assets/images/reviews/review_9.jpg";
 import review_2 from "@/assets/images/reviews/review_12.jpg";
 import review_3 from "@/assets/images/reviews/review_2.jpg";
-import review_4 from "@/assets/images/reviews/review_3.jpg";
 import review_5 from "@/assets/images/reviews/review_4.jpg";
 import review_6 from "@/assets/images/reviews/review_10.jpg";
 import review_7 from "@/assets/images/reviews/review_11.jpg";
@@ -28,12 +27,6 @@ export const reviewCardsData: ReviewCard[] = [
     },
     {
         imgSrc: review_3,
-        header: "Натяжной потолок в гостиной",
-        rate: 5,
-        message: "Наши натяжные потолки отличаются замечательным качеством и долговечностью. Этот потолок установлен в гостиной и делает ее уютной и нежной."
-    },
-    {
-        imgSrc: review_4,
         header: "Натяжной потолок в гостиной",
         rate: 5,
         message: "Наши натяжные потолки отличаются замечательным качеством и долговечностью. Этот потолок установлен в гостиной и делает ее уютной и нежной."
